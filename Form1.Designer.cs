0a34afadccfb197361a864406d39ea52d98d612c
arlindakastrati <74983643+arlindakastrati@users.noreply.github.com>
2021-05-04 15:03:44 +0200

    Add files via upload
    Creating Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,18 @@
+namespace BeaufortCipher 
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kodi i gjeneruar nga Windows Forms Designer
+        #endregion
+    }
+}
