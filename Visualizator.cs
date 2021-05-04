2a56940766e3616326cd7d57b22da300ab80a33a
Alberiana <atofaj@live.com>
2021-05-05 00:02:10 +0200

    Visualizator

---
+++ Visualizator.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BeaufortCipher 
+{
+    public class BeaufortCipher 
+    {
+        //Dritarja e udhezimeve-informatave
+        public void shfaqdritaren_informatave(){
+            String infoText = "Pasi lexuat kerkesat,shtyp ok per te perfunduar.";
+            MessageBox.Show(infoText, "Informacionet", MessageBoxButtons.OK);
+        }
+
+     
+        public void unmarkAll(){            
+        }
+       
+
+      
+    }
+}
