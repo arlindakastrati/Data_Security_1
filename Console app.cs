7127a96753f16d32fb58b7a0222b7dab49dbd332
Alberiana <atofaj@live.com>
2021-05-17 11:58:28 +0200

    Console app

--- Console app.cs
+++ Console app.cs
@@ -1,127 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-
-namespace ConsoleApp
+namespace ConsoleApp7
 {
-    class Program
+    class BeaufortCipher
     {
-        static char[,] matrix(string keyword)
+        //static void Main(string[] args)
+        public static void Main()
         {
-            char[,] matrica = new char[4, 4];
+           
+        }
 
-            StringBuilder letters = new StringBuilder();
-            for (int i = 65; i < 65 + 26; i++)
-                if ((char)i != 'K')
-                    letters.Append((char)i);
+        private static string TextSize(int length, string keytext)
+        {
+            string rez = keytext;
 
-            keyword = keyword.ToUpper();
-            keyword = keyword.Replace("K", "L");
+            int i = 0;
+            while (rez.Length < length)
+            {
+                rez += keytext[i++];
 
-            for (int i = 0; i < keyword.Length; i++)
-                letters.Replace(keyword[i].ToString(), "");
-                int k = 0;
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
+                if (i >= keytext.Length)
                 {
-                    if (k < keyword.Length)
-                    {
-                        for (int l = 0; l < k; l++)
-                        {
-                            if (k < keyword.Length && keyword[l] == keyword[k])
-                            {
-                                k++;
-                                l = -1;
-                            }
-                        }
-                        if (k < keyword.Length)
-                        {
-                            matrica[i, j] = keyword[k];
-                            k++;
-                        }
-                    }
-                    else
-                    {
-                        matrica[i, j] = letters[0];
-                        letters.Remove(0, 1);
-                    }
+                    i = 0;
                 }
-            return matrica;
-        }
-        static string Encrypt(string plaintext, char[,] matrica)
-        {
-
-            StringBuilder sb = new StringBuilder("");
-            plaintext = plaintext.Replace("K", "L");
-            plaintext = plaintext.Replace(" ", "");
-            plaintext = plaintext.ToUpper();
-
-            for (int i = 0; i < plaintext.Length; i++)
-            {
-                char chA = plaintext[i];
-                int X = 0, Y = 0;
-                for (int j = 0; j < 4; j++)
-                    for (int k = 0; k < 4; k++)
-                        if (matrica[j, k] == chA)
-                        {
-                            X = j + 1;
-                            Y = k + 1;
-                        }
-
-
-                string enc = X.ToString() + Y.ToString();
-                sb.Append(enc);
             }
 
-            return sb.ToString();
+            return rez;
         }
-        static string Decrypt(string ciphertext, char[,] matrica)
+        private static int IndexOf(char[] array, char toFind)
         {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < ciphertext.Length; i += 2)
-            {
-                char enc11 = ciphertext[i];
-                char enc21 = ciphertext[i + 1];
-                int enc1 = int.Parse(enc11.ToString()) - 1;
-                int enc2 = int.Parse(enc21.ToString()) - 1;
-                char dec = 'z';
-                for (int j = 0; j < 4; j++)
-                    for (int k = 0; k < 4; k++)
-                    {
-                        if (j == enc1 && k == enc2)
-                        {
-                            dec = matrica[j, k];
-                        }
+            int rez = -1;
 
-                    }
-                sb.Append(dec);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == toFind)
+                {
+                    rez = i;
+                    break;
+                }
             }
 
-            return sb.ToString();
+            return rez;
         }
 
-        static void Main(string[] args)
+        private static string Enkriptimi(string clearText, char[][] karakteret, string keyword)
         {
-            Console.WriteLine("Sheno plaintext-in: ");
-            string plaintext = Console.ReadLine();
+            string result = string.Empty;
 
-            Console.WriteLine("Sheno celesin: ");
-            string key = Console.ReadLine();
+            keyword = TextSize(clearText.Length, keyword);
 
+            for (int i = 0; i < clearText.Length; i++)
+            {
+                int row = keyword[i] - 'a';
+                char charToCipher = clearText[i];
 
-            char[,] matrica = matrix(key);
+                int idx = IndexOf(karakteret[row], charToCipher);
 
-            //for (int i = 0; i < 5; i++)
-            //    for (int j = 0; j < 5; j++)
-            //        Console.WriteLine(matrica[i, j]);
-            Console.WriteLine("Text-i i enkriptuar");
-            string encrypted = Encrypt(plaintext, matrica);
-            Console.WriteLine(encrypted + "\n\n");
+                if (idx == 0)
+                {
+                    idx = karakteret[row].Length;
+                }
 
-            Console.WriteLine("Text-i i dekriptuar");
-            string decrypted = Decrypt(encrypted, matrica);
-            Console.WriteLine(decrypted);
+                idx = karakteret[row].Length - idx;
 
+                result += karakteret[0][idx];
+            }
+
+            return result;
         }
-
     }
+}
+
